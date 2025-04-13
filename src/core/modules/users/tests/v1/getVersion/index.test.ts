b8d411394d87585d835127ca68023d58296c2b15
Kishor Naik <kishor.naik011.net@gmail.com>
2025-04-13 12:27:18 +0530

    @KishorNaik: Get User Row Version

--- src/core/modules/users/tests/v1/getVersion/index.test.ts
+++ src/core/modules/users/tests/v1/getVersion/index.test.ts
@@ -20,8 +20,8 @@ import { GetUserRowVersionService } from '../../../apps/feature/v1/getVersion';
 // If Debug not Worked then use
 //node --trace-deprecation --test --test-name-pattern='test_name' --require ts-node/register --inspect=4321 -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
 
-describe("Get user Entity row version", () => {
-  beforeEach(async () => {
+describe('Get user Entity row version', () => {
+	beforeEach(async () => {
 		await initializeDatabase();
 		//queryRunner = getQueryRunner();
 	});
@@ -31,14 +31,15 @@ describe("Get user Entity row version", () => {
 		await destroyDatabase();
 	});
 
-  //node --trace-deprecation --test --test-name-pattern='should_return_false_when_userEntity_identifier_is_not_provided' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
+	//node --trace-deprecation --test --test-name-pattern='should_return_false_when_userEntity_identifier_is_not_provided' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
 	test(`should_return_false_when_userEntity_identifier_is_not_provided`, async () => {
 		const userEntity: UserEntity = new UserEntity();
 		userEntity.identifier = '';
 		userEntity.status = StatusEnum.INACTIVE;
 
-		const getUserByIdentifierServiceResult =
-			await new GetUserRowVersionService().handleAsync(userEntity);
+		const getUserByIdentifierServiceResult = await new GetUserRowVersionService().handleAsync(
+			userEntity
+		);
 
 		if (getUserByIdentifierServiceResult.isErr()) {
 			expect(getUserByIdentifierServiceResult.isErr()).toBe(true);
@@ -47,14 +48,15 @@ describe("Get user Entity row version", () => {
 		expect(false).toBe(false);
 	});
 
-  //node --trace-deprecation --test --test-name-pattern='should_return_false_when_userEntity_status_is_not_provided' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
+	//node --trace-deprecation --test --test-name-pattern='should_return_false_when_userEntity_status_is_not_provided' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
 	test(`should_return_false_when_userEntity_status_is_not_provided`, async () => {
 		const userEntity: UserEntity = new UserEntity();
 		userEntity.identifier = '';
 		//userEntity.status = StatusEnum.INACTIVE;
 
-		const getUserByIdentifierServiceResult =
-			await new GetUserRowVersionService().handleAsync(userEntity);
+		const getUserByIdentifierServiceResult = await new GetUserRowVersionService().handleAsync(
+			userEntity
+		);
 
 		if (getUserByIdentifierServiceResult.isErr()) {
 			expect(getUserByIdentifierServiceResult.isErr()).toBe(true);
@@ -63,14 +65,15 @@ describe("Get user Entity row version", () => {
 		expect(false).toBe(false);
 	});
 
-  //node --trace-deprecation --test --test-name-pattern='should_return_false_when_userEntity_identifier_is_provided_wrong' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
+	//node --trace-deprecation --test --test-name-pattern='should_return_false_when_userEntity_identifier_is_provided_wrong' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
 	test(`should_return_false_when_userEntity_identifier_is_provided_wrong`, async () => {
 		const userEntity: UserEntity = new UserEntity();
 		userEntity.identifier = '754e1ad7-15c2-3f4b-ea37-83150bb826aa';
 		userEntity.status = StatusEnum.INACTIVE;
 
-		const getUserByIdentifierServiceResult =
-			await new GetUserRowVersionService().handleAsync(userEntity);
+		const getUserByIdentifierServiceResult = await new GetUserRowVersionService().handleAsync(
+			userEntity
+		);
 
 		if (getUserByIdentifierServiceResult.isErr()) {
 			expect(getUserByIdentifierServiceResult.isErr()).toBe(true);
@@ -79,23 +82,24 @@ describe("Get user Entity row version", () => {
 		expect(false).toBe(false);
 	});
 
-  //node --trace-deprecation --test --test-name-pattern='should_return_true_when_userEntity_identifier_is_provided_correct' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
+	//node --trace-deprecation --test --test-name-pattern='should_return_true_when_userEntity_identifier_is_provided_correct' --require ts-node/register -r tsconfig-paths/register ./src/core/modules/users/tests/v1/getVersion/index.test.ts
 	test(`should_return_true_when_userEntity_identifier_is_provided_correct`, async () => {
 		const userEntity: UserEntity = new UserEntity();
 		userEntity.identifier = '754e1ad7-15c2-3f4b-ea37-83150bb826ab';
 		userEntity.status = StatusEnum.INACTIVE;
 
-		const getUserByIdentifierServiceResult =
-			await new GetUserRowVersionService().handleAsync(userEntity);
+		const getUserByIdentifierServiceResult = await new GetUserRowVersionService().handleAsync(
+			userEntity
+		);
 
 		if (getUserByIdentifierServiceResult.isErr()) {
 			expect(getUserByIdentifierServiceResult.isErr()).toBe(false);
 			return;
 		}
 
-    const result=getUserByIdentifierServiceResult.value;
-    console.log(`result: ${JSON.stringify(result)}`);
+		const result = getUserByIdentifierServiceResult.value;
+		console.log(`result: ${JSON.stringify(result)}`);
 
 		expect(getUserByIdentifierServiceResult.isOk()).toBe(true);
 	});
-})
+});
