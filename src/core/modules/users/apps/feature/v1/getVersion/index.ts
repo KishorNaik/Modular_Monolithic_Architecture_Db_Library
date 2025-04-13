3cb4796fd0f9aca97b58460c40d4f2a8d871b88c
Kishor Naik <kishor.naik011.net@gmail.com>
2025-04-13 12:25:58 +0530

    @KishorNaik: Get User Row Version

---
+++ src/core/modules/users/apps/feature/v1/getVersion/index.ts
@@ -0,0 +1,15 @@
+import Container, { Service } from "typedi";
+import { sealed } from "../../../../../../shared/utils/decorators/sealed";
+import { GetByVersionIdentifierService } from "../../../../../../shared/services/db/getVersion";
+import { UserEntity } from "../../../../infrastructures/entity/tUsers";
+
+Container.set<GetByVersionIdentifierService<UserEntity>>(GetByVersionIdentifierService<UserEntity>,new GetByVersionIdentifierService<UserEntity>(UserEntity));
+
+@sealed
+@Service()
+export class GetUserRowVersionService extends GetByVersionIdentifierService<UserEntity>{
+  public constructor()
+  {
+    super(UserEntity);
+  }
+}
